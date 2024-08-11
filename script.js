ced36cdcd7080d8d423e0946f416ee25fa56935b
ammae khocan <ammarkhocan02@gmail.com>
2024-08-11 13:55:02 +0700

    Menambahkan project

---
+++ script.js
@@ -0,0 +1,39 @@
+let PasswordLenght = document.getElementById("passwordLength");
+let password = document.getElementById("password");
+let saveButton = document.getElementById("saveButton");
+
+const generatePassword = (len) => {
+  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numeric = "0123456789";
+  const symbol = "!@#$%^&*()_+=-{}[]';:/?.,<>`";
+
+  const data = lowerAlphabet + upperAlphabet + numeric + symbol;
+  let generator = "";
+  for (let index = 0; index < len; index++) {
+    generator += data[Math.floor(Math.random() * data.length)];
+  }
+  return generator;
+};
+
+const getPassword = () => {
+  const newPassword = generatePassword(PasswordLenght.value);
+  password.value = newPassword;
+  setTimeout(() => {
+    alert("password has been generated!");
+  }, 1000);
+};
+
+// save password
+const savePassword = () => {
+  document.title = password.value;
+  saveButton.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," +
+      encodeURIComponent(`password saya: ${document.title}`)
+  );
+  saveButton.setAttribute("download", "My Password Generator.txt");
+  setTimeout(() => {
+    alert("berhasil disimpan");
+  }, 1000);
+};
